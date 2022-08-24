0eb26d3f3575eaa146b7f290c95e906df3e9ecfb
Dovellous <dovelous@gmail.com>
2022-08-24 18:12:48 +0200

    Update voice call screen

--- src/dovellous/src/modules/im/sheets/im-voice-call.tsx
+++ src/dovellous/src/modules/im/sheets/im-voice-call.tsx
@@ -1,4 +1,4 @@
-import { Block, Button, Col, Link, Navbar, NavRight, NavTitle, PageContent, Row, Segmented, Sheet } from "framework7-react";
+import { Block, BlockTitle, Button, Col, Fab, FabButton, FabButtons, Icon, Link, Navbar, NavRight, NavTitle, PageContent, Row, Segmented, Sheet } from "framework7-react";
 import React, { useEffect, useState } from "react";
 
 import AgoraRTC, { IAgoraRTCClient } from "agora-rtc-sdk-ng"
@@ -234,10 +234,33 @@ export default ({ id, className, userData, isVideoCall,
             closeByOutsideClick={false}
             closeOnEscape={false}
         >
+
+            <div className="videos">
+
+            <div className="remote connected"/>
+
+            <div className="local connected"/>
+
+            </div>
             
             <PageContent>
-                    <div>Remote Videos Container</div>
-                    <div>Local Video Container</div>
+                    <div className="call-remote-user">
+                        <img src="https://images.ctfassets.net/lh3zuq09vnm2/yBDals8aU8RWtb0xLnPkI/19b391bda8f43e16e64d40b55561e5cd/How_tracking_user_behavior_on_your_website_can_improve_customer_experience.png" />
+                        <BlockTitle large>Stephen L. Gains</BlockTitle>
+                        <BlockTitle>+263 (563) 628 9828</BlockTitle>
+                    </div>
+                    <div className="call-actions">
+                        <Fab position="center-center"  >
+                            <Icon ios="f7:plus" aurora="f7:plus" md="material:add"></Icon>
+                            <Icon ios="f7:xmark" aurora="f7:xmark" md="material:close"></Icon>
+                            <FabButtons position="center">
+                                <FabButton>1</FabButton>
+                                <FabButton>2</FabButton>
+                                <FabButton>3</FabButton>
+                                <FabButton>4</FabButton>
+                            </FabButtons>
+                        </Fab>
+                    </div>
                     <Block inset className={`call-controls`}>
                         <Button outline large
                             id="im-solid-rounded-loudspeaker"
